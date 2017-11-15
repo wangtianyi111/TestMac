5682c64e9f7f8a94fa645f6f6c359669028ad26d
wangtianyi111 <932363572@qq.com>
2017-11-15 09:53:49 +0800

    jdk1.8 > jdk1.7

--- src/w/t/y/subway/TimeSorted_2.java
+++ src/w/t/y/subway/TimeSorted_2.java
@@ -1,9 +1,9 @@
 package w.t.y.subway;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
